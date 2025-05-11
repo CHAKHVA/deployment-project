dd59c63ea482685f33abf74c912dec017a13137e
chakhva <aleksandre.chakhvashvili24@gmail.com>
2025-05-11 21:54:38 +0400

    finish frontend

---
+++ frontend/src/components/AnswerDisplay/AndwerDisplay.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import './AnswerDisplay.css';
+
+const AnswerDisplay = () => {
+    const [answer, setAnswer] = useState<string>('');
+
+    useEffect(() => {
+        const fetchLatest = async () => {
+            try {
+                const res = await fetch('http://localhost:3001/api/latest-answer');
+                const data = await res.json();
+                setAnswer(data.data);
+            } catch (err) {
+                console.error('Error fetching answer:', err);
+            }
+        };
+
+        fetchLatest();
+        const interval = setInterval(fetchLatest, 2000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="card">
+            <h2 className="title">Latest Submitted Answer</h2>
+            <span id="answer" className="answer-text">
+                {answer || 'No answer yet'}
+            </span>
+        </div>
+    );
+};
+
+export default AnswerDisplay;
